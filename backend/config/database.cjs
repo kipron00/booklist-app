cdb4ba77c798b73e23b92f128d8f7f33dd551166
cypi735 <77378328+kipron00@users.noreply.github.com>
2025-07-31 07:52:57 +0300

    Update database.cjs

--- backend/config/database.cjs
+++ backend/config/database.cjs
@@ -9,7 +9,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'postgresql://booklistuser:Ka4IQlmXXQTSPq2Q8e8LuD7sKg02KooT@dpg-d25enmbuibrs73ai0bo0-a.oregon-postgres.render.com/booklist_db_3guq',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
