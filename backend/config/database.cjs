2c03d09804d1a294ced36cb63d03077645d75de8
cypi735 <77378328+kipron00@users.noreply.github.com>
2025-07-31 11:59:07 +0300

    Update database.cjs

--- backend/config/database.cjs
+++ backend/config/database.cjs
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
   development: {
-    database: process.env.DB_NAME || 'schooltrack_dev',
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || 'M1$$$ing',
+    database: process.env.DB_NAME || 'schooltrack_dev',
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres'
