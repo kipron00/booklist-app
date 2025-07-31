aac4638ca781d325c82cfb2a0097307ad33677e0
cypi735 <77378328+kipron00@users.noreply.github.com>
2025-07-31 07:39:20 +0300

    Update database.cjs

--- backend/config/database.cjs
+++ backend/config/database.cjs
@@ -1,16 +1,12 @@
 // config/database.js
-require('dotenv').config()
-
 module.exports = {
   development: {
-    use_env_variable: 'postgresql://booklistuser:Ka4IQlmXXQTSPq2Q8e8LuD7sKg02KooT@dpg-d25enmbuibrs73ai0bo0-a.oregon-postgres.render.com/booklist_db_3guq',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    database: process.env.DB_NAME || 'schooltrack_dev',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'postgresql://booklistuser:Ka4IQlmXXQTSPq2Q8e8LuD7sKg02KooT@dpg-d25enmbuibrs73ai0bo0-a.oregon-postgres.render.com/booklist_db_3guq',
